f111c33e79386e4f7623aacdc72262432462617a
juan patricio doyle <juanpatriciodoyle@gmail.com>
2025-10-03 17:47:40 +0200

    add import of css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import {CustomThemeProvider} from './contexts/ThemeContext';
 import {GlobalStyle} from './styles/globalStyles';
 import {SettingsProvider} from './utils/dx/settingsContext';
